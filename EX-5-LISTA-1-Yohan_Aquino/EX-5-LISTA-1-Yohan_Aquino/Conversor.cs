d45269d96a4d8fd1701fbc41a1739c02cdfc50cc
YohanAquino <74798062+YohanAquino@users.noreply.github.com>
2020-11-27 21:34:34 -0300

    Add files via upload

---
+++ EX-5-LISTA-1-Yohan_Aquino/EX-5-LISTA-1-Yohan_Aquino/Conversor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_5_LISTA_1_Yohan_Aquino
+{
+    class Conversor
+    {
+        private double m;
+        private double k;
+
+        public void setMilha(double x) 
+        {
+            m = x;
+        }
+        public void calcularKm()
+        {
+            k= (m * 1852) / 1000;
+        }
+        public Double getKm() 
+        {
+            return k;
+        }
+    }
+}
---
+++ EX-5-LISTA-1-Yohan_Aquino/EX-5-LISTA-1-Yohan_Aquino/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_5_LISTA_1_Yohan_Aquino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*6. Sabendo que uma milha marítima equivale a um mil, oitocentos e cinqüenta e dois metros
+e que um quilômetro possui mil metros, fazer um programa para converter milhas marítimas em
+quilômetros. */
+            Conversor con = new Conversor();
+
+            Console.WriteLine("Digite um valor, em milhas: ");
+            con.setMilha(double.Parse(Console.ReadLine()));
+
+            con.calcularKm();
+            Console.WriteLine("O valor em quilômetros é {0} km",con.getKm());
+
+            Console.ReadKey();
+        }
+    }
+}
